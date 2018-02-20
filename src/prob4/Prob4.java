60b3e2134e7ec73bf9b5b4a62f7acf7aa36470e9
bit <bit@DESKTOP-518UE4O>
2018-02-20 16:15:53 +0900

    p4 solved

--- src/prob4/Prob4.java
+++ src/prob4/Prob4.java
@@ -8,4 +8,24 @@ public class Prob4 {
 		char[] c2 = reverse( "Java Programming!" );
 		printCharArray( c2 );
 	}
+	
+	public static char[] reverse(String str){
+		if(str == null) return null;
+		
+		char[] tmp = str.toCharArray();
+		char[] result = new char[str.length()];
+		
+		for(int i = 0 ; i < result.length; i++){
+			result[i] = tmp[tmp.length-i-1];
+		}
+		
+		return result;
+	}
+	
+	public static void printCharArray(char[] array){
+		for(char c : array){
+			System.out.print(c);
+		}
+		System.out.println("");
+	}
 }
