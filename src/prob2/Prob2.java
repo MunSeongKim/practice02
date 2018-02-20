bcfbdd6eebcd467c85cd43944c93ffe858fe9ebc
bit <bit@DESKTOP-518UE4O>
2018-02-20 16:06:49 +0900

    p2 solved

--- src/prob2/Prob2.java
+++ src/prob2/Prob2.java
@@ -6,8 +6,16 @@ public class Prob2 {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner( System.in );
+		int intArray[] = new int[5];
+		double sum = 0;
 		
 		System.out.println( "5개의 숫자를 입력하세요." );
+		for(int i = 0; i < intArray.length; i++){
+			intArray[i] = scanner.nextInt();
+			sum += intArray[i];
+		}
+		
+		System.out.println("평균은 " + (sum / intArray.length) + " 입니다.");
 		
 		scanner.close();
 		
